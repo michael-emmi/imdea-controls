f61e8ca9b1f6bf26057a47f39bd961aab74f5f34
Michael Emmi <michael.emmi@gmail.com>
2013-09-25 14:22:52 +0200

    Finished the blinds script.

--- blinds.rb
+++ blinds.rb
@@ -1,5 +1,7 @@
-#!/usr/bin/env ruby
+#!/usr/local/bin/ruby
+require 'rubygems'
 require 'mechanize'
+require 'optparse'
 
 class Controls
   URI = 'http://control.imdeasoftware.org/screenmate/ScreenMateChangeValuePage.aspx'
@@ -8,10 +10,12 @@ class Controls
   BLINDS_ID = '3884056604'
   TEMP_ID = '3884056589'
   
+  DEFAULT_BLINDS = 0
+  DEFAULT_TEMP = 24.5
+  
   def initialize(room)
     @agent = Mechanize.new
     @room = room
-    @room_id = "PLANTA_#{room / 100}-Despacho_#{room % 100}"
 
     # begin the session
     form = @agent.get(URI).form
@@ -25,28 +29,60 @@ class Controls
     form.roomId = room
     @agent.submit(form, form.buttons_with(:name => /lookUpRoomId/).first)
   end
-  
+
   private
   
+  def room_id
+    "PLANTA_#{@room / 100}-Despacho_#{@room % 100}"
+  end
+  
   def control(obj, val)
-    form = @agent.get(URI + "?objectIdRoot=#{@room_id}&objectId=#{obj}").form
+    form = @agent.get(URI + "?objectIdRoot=#{room_id}&objectId=#{obj}").form
     form.newValue = val
     @agent.submit(form, form.buttons_with(:name => /saveButton/).first)
+    nil
   end
   
   public
   
-  def blinds(val = 0)
-    control(BLINDS_ID, val)
+  def self.cmdline(args)
+    room = nil
+    blinds = nil
+    temp = nil
+    
+    OptionParser.new do |opts|
+      opts.banner = "Usage: #{File.basename $0} [options]"
+      opts.on("-r", "--room ROOM", Integer, "Set the ROOM number") do |n|
+        room = n
+      end
+      opts.on("-b", "--blinds NUM", Integer, "Set the blinds to NUM") do |n|
+        blinds = n
+      end
+      opts.on("-t", "--temp TEMP", Float, "Set the TEMPerature") do |n|
+        temp = n
+      end
+    end.parse!(args)
+    
+    abort("Must specify a room number.") unless room
+        
+    c = Controls.new(room) if blinds || temp
+    c.blinds(blinds) if blinds
+    c.temp(temp) if temp
   end
   
-  def temp(val = 24.0)
+  def blinds(val = DEFAULT_BLINDS)
+    control(BLINDS_ID, val)
+    puts "blinds set to #{val}"
+  end
+  
+  def temp(val = DEFAULT_TEMP)
     control(TEMP_ID, val)
+    puts "temperature set to #{val}"
   end
 end
 
-room = 358
-blinds = ARGV[0] 
-  
-c = Controls.new(room)
-c.blinds(ARGV[0])
+# only when this script is invoked from from the command line
+if __FILE__ == $0
+  Controls.cmdline(ARGV)
+end
+
