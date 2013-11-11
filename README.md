b7dad21eb0cb69b44f04af647d28e5bb05db9a1c
Michael Emmi <michael.emmi@gmail.com>
2013-11-11 11:18:48 +0100

    Using configuration files.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 imdea-controls
 ==============
 
-A script to control the room temperature and blinds at the IMDEA Software Institute.
+A script to control the room temperature, blinds, and lights at the IMDEA
+Software Institute.
 
 Dependencies
 ------------
@@ -14,9 +15,26 @@ Usage
 -----
 
     blinds.rb [options]
-        -r, --room ROOM                  Set the ROOM number
-        -b, --blinds NUM                 Set the blinds to NUM
-        -t, --temp TEMP                  Set the TEMPerature
+    -u, --username USERNAME          Your USERNAME
+    -p, --password PASSWORD          Your PASSWORD
+    -r, --room ROOM                  Set the ROOM number
+    -b, --blinds NUM                 Set the blinds to NUM
+    -t, --temp TEMP                  Set the TEMPerature
+    -w, --window LIGHT               Set the window LIGHT
+    -d, --door LIGHT                 Set the door LIGHT
+    -l, --lights LIGHT               Set all the LIGHTS
+    -v, --verbose
+
+Configuration file
+------------------
+
+`blinds.rb` looks for `.blinds.yml` in your home directory `~` and your current
+directory `.`.  If `.blinds.yml` exists, `blinds.rb` will read your USERNAME,
+PASSWORD, and ROOM.  `.blinds.yml` has the following format:
+
+    username: USERNAME
+    password: PASSWORD
+    room_no: ROOM
 
 Author
 ------
--- blinds.rb
+++ blinds.rb
@@ -2,32 +2,56 @@
 require 'rubygems'
 require 'mechanize'
 require 'optparse'
+require 'yaml'
+
+$loud = false
 
 class Controls
   URI = 'http://control.imdeasoftware.org/screenmate/ScreenMateChangeValuePage.aspx'
-  USERNAME = 'imdea'
-  PASSWORD = 'imdea'  
-  BLINDS_ID = '3884056604'
-  TEMP_ID = '3884056589'
   
-  DEFAULT_BLINDS = 0
-  DEFAULT_TEMP = 24.5
+  IDS = {
+    temperature_setting: '3884056589',
+    sunblind_control: '3884056587',
+    sunblind_setting: '3884056604',
+    door_light_control: '3884056590',
+    door_light_setting: '3884056581',
+    window_light_control: '3884056601',
+    window_light_setting: '3884056602'
+  }
   
-  def initialize(room)
+  DEFAULTS = {
+    sunblind: 0,
+    temperature: 24.5,
+    window_light: 0,
+    door_light: 0
+  }
+  
+  def initialize(username, password, room)
     @agent = Mechanize.new
+    @username = username
+    @password = password
     @room = room
 
     # begin the session
+    print "opening session... " if $loud
     form = @agent.get(URI).form
+    puts "OK" if $loud && form
+    abort "Could not open session." unless form
 
     # login
-    form.userName = USERNAME
-    form.password = PASSWORD
+    form.userName = @username
+    form.password = @password
+    print "logging in... " if $loud
     form = @agent.submit(form, form.buttons_with(:name => /loginButton/).first).form
+    puts "OK" if $loud && form
+    abort "could not log in as #{@username}." unless form
     
     # choose the room
-    form.roomId = room
-    @agent.submit(form, form.buttons_with(:name => /lookUpRoomId/).first)
+    form.roomId = @room
+    print "selecting room... " if $loud
+    page = @agent.submit(form, form.buttons_with(:name => /lookUpRoomId/).first)
+    puts "OK" if $loud && page
+    abort "could not select room #{room}." unless page
   end
 
   private
@@ -39,7 +63,10 @@ class Controls
   def control(obj, val)
     form = @agent.get(URI + "?objectIdRoot=#{room_id}&objectId=#{obj}").form
     form.newValue = val
-    @agent.submit(form, form.buttons_with(:name => /saveButton/).first)
+    print "setting controls... " if $loud
+    page = @agent.submit(form, form.buttons_with(:name => /saveButton/).first)
+    puts "OK" if $loud && page
+    abort "control command failed." unless page
     nil
   end
   
@@ -49,9 +76,29 @@ class Controls
     room = nil
     blinds = nil
     temp = nil
+    window = nil
+    door = nil
+    username = nil
+    password = nil
+    
+    cfg = {}
+    ['~','.'].each do |path|
+      cfgfile = File.join(path, '.blinds.yml')
+      if File.exists? cfgfile then
+        # puts "Loading configuration file #{cfgfile}"
+        cfg = YAML::load_file(cfgfile)
+        break
+      end
+    end
     
     OptionParser.new do |opts|
       opts.banner = "Usage: #{File.basename $0} [options]"
+      opts.on("-u", "--username USERNAME", "Your USERNAME") do |n|
+        username = n
+      end
+      opts.on("-p", "--password PASSWORD", "Your PASSWORD") do |n|
+        password = n
+      end
       opts.on("-r", "--room ROOM", Integer, "Set the ROOM number") do |n|
         room = n
       end
@@ -61,28 +108,43 @@ class Controls
       opts.on("-t", "--temp TEMP", Float, "Set the TEMPerature") do |n|
         temp = n
       end
+      opts.on("-w", "--window LIGHT", Float, "Set the window LIGHT") do |n|
+        window = n
+      end
+      opts.on("-d", "--door LIGHT", Float, "Set the door LIGHT") do |n|
+        door = n
+      end
+      opts.on("-l", "--lights LIGHT", Float, "Set all the LIGHTS") do |n|
+        window = n
+        door = n
+      end
+      opts.on("-v", "--verbose") do |v|
+        $loud = v
+      end
     end.parse!(args)
     
+    username ||= cfg['username']
+    password ||= cfg['password']
+    room ||= cfg['room_no'].to_i
+    
+    abort("Must specify a username.") unless username
+    abort("Must specify a password.") unless password
     abort("Must specify a room number.") unless room
-        
-    c = Controls.new(room) if blinds || temp
-    c.blinds(blinds) if blinds
-    c.temp(temp) if temp
+    
+    c = Controls.new(username, password, room) if blinds || temp || window || door
+    c.sunblind(blinds) if blinds
+    c.temperature(temp) if temp
+    c.window_light(window) if window
+    c.door_light(door) if door
   end
   
-  def blinds(val = DEFAULT_BLINDS)
-    control(BLINDS_ID, val)
-    puts "blinds set to #{val}"
-  end
-  
-  def temp(val = DEFAULT_TEMP)
-    control(TEMP_ID, val)
-    puts "temperature set to #{val}"
+  [:sunblind, :temperature, :window_light, :door_light].each do |x|
+    define_method(x) do |val|
+      val ||= DEFAULTS[x]
+      control( IDS[:"#{x}_setting"], val )
+      puts "#{x.to_s.gsub(/_/,' ')} set to #{val}"
+    end
   end
 end
 
-# only when this script is invoked from from the command line
-if __FILE__ == $0
-  Controls.cmdline(ARGV)
-end
-
+Controls.cmdline(ARGV) if __FILE__ == $0
